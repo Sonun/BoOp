996f4382c8b9a63eaa7f3f903b78be856e5e469b
Gadoffelbrei <Gadoffelbrei@DESKTOP-ENK27CA>
2021-09-01 23:52:29 +0200

    View Model Delegate Command Viewmodels Ordnerstruktur

---
+++ BoOp.UI.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoOp.UI.WPF.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _bookname;
+        private string _username;
+
+        public DelegateCommand ClearCommand { get; set; }
+
+        public string Bookname { 
+            get => _bookname; 
+            set
+            {
+                _bookname = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(UserOwnsBook));
+                ClearCommand.OnExecuteChanged();
+            }
+        }
+        public string Username {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(UserOwnsBook));
+                ClearCommand.OnExecuteChanged();
+            }
+        }
+
+        public string UserOwnsBook => $"{Username} gehört {Bookname}";
+
+        public MainWindowViewModel()
+        {
+            ClearCommand = new DelegateCommand(
+                (o) =>
+                {
+                    return !string.IsNullOrEmpty(Username) || !string.IsNullOrEmpty(Bookname);
+                },
+
+                (o) =>
+                {
+                    Bookname = "";
+                    Username = "";
+                }
+            );
+
+            Bookname = "book";
+            Username = "user";
+        }
+    }
+}
---
+++ BoOp.UI.WPF/ViewModels/ViewModelUtils/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BoOp.UI.WPF.ViewModels
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public DelegateCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void OnExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute?.Invoke(parameter);
+    }
+}
