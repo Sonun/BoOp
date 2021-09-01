873c73c34b668c2f11b9502f944e80f969397eb4
Manuel Janzen <manuel.janzen@sinc.de>
2021-09-01 12:37:07 +0200

    Projektdateien hinzufügen.

---
+++ BoOp.UIConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoOp.UIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
