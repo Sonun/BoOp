84a8fa99d6af0085600d0cdfa405c957d3b373de
Manuel Janzen <manuel.janzen@sinc.de>
2021-09-01 14:33:49 +0200

    Grundarchitektur geschaffen

---
+++ BoOp.Business/ILibrary.cs
@@ -0,0 +1,17 @@
+﻿using BoOp.Business.Models;
+
+namespace BoOp.Business
+{
+    public interface ILibrary
+    {
+        // Dependent on login
+        public void LendBook(User user, Book book);
+
+        // Independent on login
+        public void ReturnBook(Book book);
+
+        // Dependent on User Rights
+        public void AddBook(Book book);
+
+    }
+}
---
+++ BoOp.Business/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoOp.Business.Models
+{
+    public class User
+    {
+        public enum Rights
+        {
+            Leser, // Reservieren, Ausleihen, Zurückgeben
+            Schulhelfer, // Ausleihen und Zurückgeben für andere, Bücher und Benutzer anlegen (nicht löschen)
+            BiboTeam, // wie Schulhelfer + Löschen von Büchern und Benutzer möglich
+            Admin // Alles (BiboTeam anlegen und löschen)
+        }
+
+        public string ID { get; set; }
+        public Rights Right { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
