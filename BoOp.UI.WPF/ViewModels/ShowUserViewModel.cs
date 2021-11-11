cc15ebb96a7b9ed262fe58f0c9a3dba1c1675472
Manuel Janzen <manuel.janzen@sinc.de>
2021-11-11 09:23:21 +0100

    Konto anzeigen

--- BoOp.UI.WPF/ViewModels/ShowUserViewModel.cs
+++ BoOp.UI.WPF/ViewModels/ShowUserViewModel.cs
@@ -14,22 +14,40 @@ namespace BoOp.UI.WPF.ViewModels
     {
         private readonly INavigationService _navigationService;
         private readonly PersonModel _logedinUser;
+        private readonly LibraryViewModel _libraryViewModel;
+        private bool _showUserDetailsView;
+        private bool _backToLibrary;
 
+        public bool ShowUserDetailsView { get { return _showUserDetailsView; } set { _showUserDetailsView = value; OnPropertyChanged(); } }
         public PersonModel PersonModel { get; set; }
         public ObservableCollection<ExemplarViewModel> LendedBooks { get; set; }
-        public DelegateCommand BackCommand { get; set; }    
+        public DelegateCommand BackCommand { get; set; }
 
-        public ShowUserViewModel(INavigationService navigationService, PersonModel editor,PersonModel user, List<ExemplarViewModel> exemplarViewModels)
+        public ShowUserViewModel()
         {
+            ShowUserDetailsView = false;
+        }
+        public ShowUserViewModel(INavigationService navigationService, PersonModel editor,PersonModel user, List<ExemplarViewModel> exemplarViewModels, LibraryViewModel libraryViewModel = null, bool backToLibrary = false)
+        {
+            ShowUserDetailsView = true;
+            _backToLibrary = backToLibrary;
             LendedBooks = new ObservableCollection<ExemplarViewModel>();
             PersonModel = user;
+            _libraryViewModel = libraryViewModel;
             exemplarViewModels.ForEach(x => LendedBooks.Add(x));
             _navigationService = navigationService;
             _logedinUser = editor;
 
             BackCommand = new DelegateCommand(x =>
             {
-                _navigationService.ShowAdminView(editor);
+                if (_backToLibrary)
+                {
+                    _libraryViewModel.ShowUserViewModel = new ShowUserViewModel();
+                }
+                else
+                {
+                    _navigationService.ShowAdminView(editor);
+                }
             });
         }
     }
